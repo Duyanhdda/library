7e8cc27b9efc3ae99bc1cee7c383fdd7eab4aa5e
Duyanhdda <anh.haduyanh@hcmut.edu.vn>
2021-12-27 21:47:06 +0700

    update

--- src/modules/auth/google.strategy.ts
+++ src/modules/auth/google.strategy.ts
@@ -15,7 +15,7 @@ export class GoogleStrategy extends PassportStrategy(Strategy, 'google') {
     super({
       clientID: process.env.GOOGLE_CLIENT_ID,
       clientSecret: process.env.GOOGLE_SECRET,
-      callbackURL: 'http://localhost:3000/google/callback',
+      callbackURL: 'https://mysterious-basin-07133.herokuapp.com/google/callback',
       scope: ['email', 'profile'],
     });
   }
